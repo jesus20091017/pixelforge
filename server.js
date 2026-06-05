5227a28c18e14133c6dae3f4b710b0577c1f4f75
jesu hdz <jakskskk10@gmail.com>
2026-06-04 23:22:38 -0500

    Configurar puerto para Render

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.post("/producto", async (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Servidor iniciado en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en puerto ${PORT}`);
 });
