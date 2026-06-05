1f18993247959b656e93b04e94f118e6ecb21cb0
jesu hdz <jakskskk10@gmail.com>
2026-06-05 00:14:15 -0500

    Arreglar package.json para Render

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Si NO quieres renombrar tu archivo, usa esto:
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "práctica.html"));
+    res.sendFile(path.join(__dirname, "public", "practica.html"));
 });
 
 // ===== MONGO =====
